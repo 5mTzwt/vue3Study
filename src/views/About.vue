90a84d2e8a960968fe966af27fc9fa31e1bc96b3
卖馒头的小花卷 <2369862600@qq.com>
2021-04-03 20:16:33 +0800

    feat: 数据和方法

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,43 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>{{ msg }}</p>
+    <p>{{ msg2 }}</p>
+    <p>{{ msg3 }}</p>
+    <button @click="changeData">change</button>
   </div>
 </template>
+<script>
+import { reactive, ref, toRefs } from "@vue/composition-api";
+export default {
+  /**
+   * vue2.x
+   */
+  data() {
+    return {
+      msg: "对不起，我是一个警察",
+    };
+  },
+  /**
+   * vue3.0
+   */
+  setup() {
+    const state = reactive({
+      msg2: "你是一个坏蛋",
+    });
+    // return state;
+    const msg3 = ref("加油！！");
+    // 方法
+    const changeData = () => {
+      console.log("METHODS");
+      state.msg2 = "你是一个大坏蛋";
+      msg3.value = "雅玛多"; // 显式修改
+    };
+    return {
+      msg3,
+      ...toRefs(state),
+      changeData,
+    };
+  },
+};
+</script>
